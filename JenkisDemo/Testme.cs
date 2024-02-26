444f07eb5a860ec548d1c5ca6326a296c493910e
Laevi Jerisha Antony Amal Raj <laevi.antony@itp.objectfrontier.com>
2024-02-26 10:34:04 +0530

    First Commit

---
+++ JenkisDemo/Testme.cs
@@ -0,0 +1,16 @@
+﻿namespace JenkisDemo
+{
+    public class Testme
+    {
+        public String sayJenkins(int times)
+
+        {
+            string Output = "";
+            for (int i = 0; i < times; i++)
+            {
+                Output += "Jenkins";
+            }
+            return Output;
+        }
+    }
+}
